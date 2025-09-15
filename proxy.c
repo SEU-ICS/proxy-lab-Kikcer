12060315c8f3300ac93a7fd68370ed5f18265c3f
xxx <91067875+Marshal1l@users.noreply.github.com>
2025-09-15 09:12:14 +0800

    Update proxy.c

--- proxy.c
+++ proxy.c
@@ -3,7 +3,7 @@
  #include <pthread.h>
  #include <ctype.h>
  #include <string.h>
- 
+ //
 /* Recommended max cache and object sizes */
  #define MAX_CACHE_SIZE 1049000      
  #define MAX_OBJECT_SIZE 102400      //单对象最大可缓存字节
